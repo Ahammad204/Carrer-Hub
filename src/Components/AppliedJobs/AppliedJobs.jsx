ca8d3e6e4c6c3d14bd662b547d26507fa04fe746
Ahammad204 <Ahammadcode@gmail.com>
2023-09-23 09:09:36 +0600

    applied jobs dynamic

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../Utility/localStorage";
 
@@ -7,20 +7,57 @@ const AppliedJobs = () => {
 
     const jobs = useLoaderData()
 
-    useEffect(()=>{
+    const [appliedJobs, setAppliedJobs] = useState([])
+
+    useEffect(() => {
 
         const storedJobIds = getStoredJobApplication()
-        if(jobs.length > 0){
+        if (jobs.length > 0) {
 
-            const JobsApplied = jobs.filter(job => )
+            // const JobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
+            const JobsApplied = []
+            for (const id of storedJobIds) {
 
+                const job = jobs.find(job => job.id === id)
+                if (job) {
+
+                    JobsApplied.push(job)
+
+                }
+
+
+            }
+
+            setAppliedJobs(JobsApplied)
         }
 
-    },[])
+    }, [])
 
     return (
         <div>
-            <h2>Jobs I applied</h2>
+            <h2 className="text-2xl ">Jobs I applied in {appliedJobs.length}</h2>
+
+            <details className="dropdown mb-32">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li><a>All</a></li>
+                    <li><a>Remote</a></li>
+                    <li><a>Onsite</a></li>
+                </ul>
+            </details>
+
+            <ul>
+                {
+
+                    appliedJobs.map(job => <li key={job.id}>
+
+                        <span>{job.job_title} {job.company_name}: {job.remote_or_onsite} </span>
+
+                    </li>)
+
+                }
+            </ul>
+
         </div>
     );
 };
