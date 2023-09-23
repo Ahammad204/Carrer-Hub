8d879036b6ad671813ab3d442d973d4a30b1f139
Ahammad204 <Ahammadcode@gmail.com>
2023-09-23 07:26:36 +0600

    design featured jos

--- src/Components/FeturedJobs/FeturedJobs.jsx
+++ src/Components/FeturedJobs/FeturedJobs.jsx
@@ -5,6 +5,7 @@ import Job from "../job/Job";
 const FeturedJobs = () => {
 
     const [jobs,setJobs] = useState([])
+    const [dataLength,setDataLength] = useState(4)
 
     useEffect(()=>{
 
@@ -17,15 +18,19 @@ const FeturedJobs = () => {
     return (
         <div>
             <div className="text-center">
-                <h1 className="text-5xl">Featured Jobs: {jobs.length}</h1>
+                <h1 className="text-5xl">Featured Jobs</h1>
                 <p >Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-2 gap-6">
                 {
 
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0,dataLength).map(job => <Job key={job.id} job={job}></Job>)
 
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                
+                <button onClick={()=> setDataLength(jobs.length)} className="btn btn-primary bg-gradient-to-r from-blue-500 to-purple-500 border-none">Show All Jobs</button>
             </div>
         </div>
     );
